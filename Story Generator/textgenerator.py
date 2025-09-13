dfbb33a5e28ac6b9b1bf3dedbea5079f8b42125d
Sujal-Sawalkar <sujalsawalkar2005@gmail.com>
2025-09-13 23:55:57 +0530

    Add files via upload

---
+++ Story Generator/textgenerator.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+
+generator = pipeline("text-generation", model = "gpt2")
+
+prompt = input("Input the text you want to make story on:")
+len = int(input("Input the length of the story you want to generate:"))
+
+result = generator(prompt, max_length = len, num_return_sequences = 1,max_new_tokens=700,
+    truncation=True,
+    pad_token_id=50256,
+    temperature=0.9,   # higher = more creative
+    top_p=0.95)
+
+print(result[0]['generated_text']) 
